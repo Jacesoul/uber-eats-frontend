d2edc379f66f3c206bd7a098ef2ec26c5d43a462
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-03-26 19:31:42 +0900

    Local only Fields & Reactive variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useReactiveVar } from "@apollo/client";
 import React from "react";
+import { isLoggedInVar } from "./apollo";
+import { LoggedInRouter } from "./routers/logged-in-router";
 import { LoggedOutRouter } from "./routers/logged-out-router";
 
 function App() {
-  return <LoggedOutRouter></LoggedOutRouter>;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? (
+    <LoggedInRouter></LoggedInRouter>
+  ) : (
+    <LoggedOutRouter></LoggedOutRouter>
+  );
 }
 
 export default App;
