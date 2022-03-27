954237ffc156462d4a97814fb3e3b439cdbff51a
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-03-27 14:09:31 +0900

    Apollo Codegen

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,9 +2,13 @@ import { gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
 import { FormError } from "../components/form-error";
+import {
+  loginMutation,
+  loginMutationVariables,
+} from "../__generated__/loginMutation";
 
 const LOGIN_MUTAION = gql`
-  mutation PotatoMutation($email: String!, $password: String!) {
+  mutation loginMutation($email: String!, $password: String!) {
     login(input: { email: $email, password: $password }) {
       ok
       error
@@ -20,7 +24,10 @@ interface ILoginForm {
 
 export const Login = () => {
   const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
-  const [loginMutation, { loading, error, data }] = useMutation(LOGIN_MUTAION);
+  const [loginMutation, { loading, error, data }] = useMutation<
+    loginMutation,
+    loginMutationVariables
+  >(LOGIN_MUTAION);
   const onSubmit = () => {
     const { email, password } = getValues();
     loginMutation({
