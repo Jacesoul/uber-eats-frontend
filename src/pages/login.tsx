046ac346c763496ed7cad384088c77b841eee8f6
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-03-27 12:47:25 +0900

    Login Mutation part One

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,34 @@
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+
+const LOGIN_MUTAION = gql`
+  mutation PotatoMutation($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      ok
+      error
+      token
+    }
+  }
+`;
 
 interface ILoginForm {
-  email?: string;
-  password?: string;
+  email: string;
+  password: string;
 }
 
 export const Login = () => {
   const { register, getValues, errors, handleSubmit } = useForm<ILoginForm>();
+  const [loginMutation, { loading, error, data }] = useMutation(LOGIN_MUTAION);
   const onSubmit = () => {
-    console.log(getValues());
+    const { email, password } = getValues();
+    loginMutation({
+      variables: {
+        email,
+        password,
+      },
+    });
   };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
@@ -28,9 +47,7 @@ export const Login = () => {
             className="input"
           ></input>
           {errors.email?.message && (
-            <span className=" text-red-400 font-medium">
-              {errors.email?.message}
-            </span>
+            <FormError errorMessage={errors.email?.message}></FormError>
           )}
           <input
             ref={register({ required: "Password is required", minLength: 10 })}
@@ -41,14 +58,10 @@ export const Login = () => {
             className="input"
           ></input>
           {errors.password?.message && (
-            <span className=" text-red-400 font-medium">
-              errors.password?.message
-            </span>
+            <FormError errorMessage={errors.password?.message}></FormError>
           )}
           {errors.password?.type === "minLength" && (
-            <span className=" text-red-400 font-medium">
-              Password must be more than 10 chars.
-            </span>
+            <FormError errorMessage="Password must be more than 10 chars."></FormError>
           )}
           <button className="btn mt-3">Login</button>
         </form>
