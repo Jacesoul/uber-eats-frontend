5029a7651241eb201f62386be54d67a8301107e4
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-03-29 12:17:26 +0900

    Verifying Email part One

--- src/components/headers.tsx
+++ src/components/headers.tsx
@@ -2,21 +2,32 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useMe } from "../hooks/useMe";
 import uberLogo from "../images/uber-eats-logo.svg";
 
 interface IHeaderProps {
   email: string;
 }
 
-export const Header: React.FC<IHeaderProps> = ({ email }) => (
-  <header className=" py-1">
-    <div className=" w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
-      <img src={uberLogo} className="w-24" alt="Uber Eats"></img>
-      <span className=" text-xs">
-        <Link to="/my-profile">
-          <FontAwesomeIcon icon={faUser} className=" text-xl" />
-        </Link>
-      </span>
-    </div>
-  </header>
-);
+export const Header: React.FC<IHeaderProps> = ({ email }) => {
+  const { data } = useMe();
+  return (
+    <>
+      {!data?.me.verified && (
+        <div className=" bg-red-500 py-3 px-3 text-center text-xs text-white">
+          <span>Please verify your email.</span>
+        </div>
+      )}
+      <header className=" py-1">
+        <div className=" w-full px-5 xl:px-0 max-w-screen-xl mx-auto flex justify-between items-center">
+          <img src={uberLogo} className="w-24" alt="Uber Eats"></img>
+          <span className=" text-xs">
+            <Link to="/my-profile">
+              <FontAwesomeIcon icon={faUser} className=" text-xl" />
+            </Link>
+          </span>
+        </div>
+      </header>
+    </>
+  );
+};
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -8,9 +8,12 @@ import {
 import { Restaurants } from "../pages/client/restaurants";
 import { Header } from "../components/headers";
 import { useMe } from "../hooks/useMe";
+import { NotFound } from "../pages/404";
+import { ConFirmEmail } from "../pages/user/confirm-email";
 
 const ClientRoutes = () => [
   <Route path="/" element={<Restaurants></Restaurants>}></Route>,
+  <Route path="/confirm" element={<ConFirmEmail></ConFirmEmail>}></Route>,
 ];
 
 export const LoggedInRouter = () => {
@@ -27,7 +30,7 @@ export const LoggedInRouter = () => {
       <Header email={data.me.email}></Header>
       <Routes>
         {data.me.role === "Client" && ClientRoutes()}
-        <Route path="*" element={<Navigate to="/" replace></Navigate>}></Route>
+        <Route path="/*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </Router>
   );
