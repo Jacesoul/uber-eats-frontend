fbc5f70bd25ff50dcf408473930367bae146f8ff
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-03-30 11:57:15 +0900

    Restaurants Query

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,5 +1,6 @@
 import { gql, useApolloClient, useMutation } from "@apollo/client";
 import React, { useEffect } from "react";
+import { Helmet } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
 import { useMe } from "../../hooks/useMe";
 import {
@@ -47,6 +48,9 @@ export const ConFirmEmail = () => {
   }, []);
   return (
     <div className=" mt-52 flex flex-col items-center justify-center">
+      <Helmet>
+        <title>Verify Email | Uber Eats</title>
+      </Helmet>
       <h2 className=" text-lg mb-1 font-medium">Confirming email...</h2>
       <h4 className=" text-gray-700 text-sm">
         Please wait, don't close the page...
