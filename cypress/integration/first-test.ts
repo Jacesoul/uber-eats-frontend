d105a03ccb817ae34515cb144edb88534a5107f4
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-04-07 17:07:54 +0900

    Login E2E

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -3,16 +3,33 @@ describe("Log In", () => {
   it("should see login page", () => {
     user.visit("/").title().should("eq", "Login | Uber Eats");
   });
-  it("can fill out the form", () => {
-    user.visit("/");
-    user.findByPlaceholderText(/email/i).type("jace@naver.com");
-    user.findByPlaceholderText(/password/i).type("1q2w3e4r5t");
-    user.findByRole("button").should("not.have.class", "pointer-events-none");
-  });
 
   it("can see email / password validation errors", () => {
     user.visit("/");
     user.findByPlaceholderText(/email/i).type("bade@email");
     user.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole("alert").should("have.text", "Email is required");
+    user.findByPlaceholderText(/email/i).type("bade@email.com");
+    user
+      .findByPlaceholderText(/password/i)
+      .type("a")
+      .clear();
+    user.findByRole("alert").should("have.text", "Password is required");
+  });
+
+  it("can fill out the form", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("jace123@naver.com");
+    user.findByPlaceholderText(/password/i).type("1q2w3e4r5t");
+    user
+      .findByRole("button")
+      .should("not.have.class", "pointer-events-none")
+      .click();
+    user.window().its("localStorage.uber-token").should("be.a", "string");
+  });
+
+  it("sign up", () => {
+    user.visit("/create-accout");
   });
 });
