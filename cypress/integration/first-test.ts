e39499cc62c4f1f983d6dc625722a6bb06ee5de4
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-04-07 16:53:38 +0900

    Cypress Testing Library

--- cypress/integration/first-test.ts
+++ cypress/integration/first-test.ts
@@ -1,5 +1,18 @@
-describe("First Test", () => {
-  it("should go to homepage", () => {
-    cy.visit("http://localhost:3000").title().should("eq", "Login | Uber Eats");
+describe("Log In", () => {
+  const user = cy;
+  it("should see login page", () => {
+    user.visit("/").title().should("eq", "Login | Uber Eats");
+  });
+  it("can fill out the form", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("jace@naver.com");
+    user.findByPlaceholderText(/password/i).type("1q2w3e4r5t");
+    user.findByRole("button").should("not.have.class", "pointer-events-none");
+  });
+
+  it("can see email / password validation errors", () => {
+    user.visit("/");
+    user.findByPlaceholderText(/email/i).type("bade@email");
+    user.findByRole("alert").should("have.text", "Please enter a valid email");
   });
 });
