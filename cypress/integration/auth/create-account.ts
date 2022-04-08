e173344af0de61e5eca105cd2c1762a27a120d74
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-04-08 13:49:18 +0900

    Create Account E2E part Two

--- cypress/integration/auth/create-account.ts
+++ cypress/integration/auth/create-account.ts
@@ -14,13 +14,31 @@ describe("Create Account", () => {
       .clear();
     user.findByRole("alert").should("have.text", "Password is required");
   });
+
   it("should be able to create account and login", () => {
+    user.intercept("http://localhost:4000/graphql", (req) => {
+      const { operationName } = req.body;
+      if (operationName && operationName === "createAccountMutation") {
+        req.reply((res) => {
+          res.send({
+            data: {
+              createAccount: {
+                ok: true,
+                error: null,
+                __typename: "CreateAccountOutput",
+              },
+            },
+          });
+        });
+      }
+    });
     user.visit("/create-account");
-    user.findByPlaceholderText(/email/i).type("real3@email.com");
+    user.findByPlaceholderText(/email/i).type("jace123@naver.com");
     user.findByPlaceholderText(/password/i).type("1q2w3e4r5t");
     user.findByRole("button").click();
     user.wait(1000);
-    user.findByPlaceholderText(/email/i).type("real3@email.com");
+    user.title().should("eq", "Login | Uber Eats");
+    user.findByPlaceholderText(/email/i).type("jace123@naver.com");
     user.findByPlaceholderText(/password/i).type("1q2w3e4r5t");
     user.findByRole("button").click();
     user.window().its("localStorage.uber-token").should("be.a", "string");
