3505d5038549cddc65ab4a2a489636f3317fa92e
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-04-09 14:53:52 +0900

    EditProfile E2E part Two & Intercept & Fixtures

--- cypress/integration/user/edit-profile.tsx
+++ cypress/integration/user/edit-profile.tsx
@@ -2,7 +2,7 @@ describe("Edit Profile", () => {
   const user = cy;
   beforeEach(() => {
     // @ts-ignore
-    user.login("jace12345@naver.com", "1q2w3e4r5t");
+    user.login("jace@naver.com", "1q2w3e4r5t");
   });
 
   it("can go to /edit-profile using the header", () => {
@@ -12,6 +12,12 @@ describe("Edit Profile", () => {
   });
 
   it("can change email", () => {
+    user.intercept("POST", "http://localhost:4000/graphql", (req) => {
+      console.log(req.body);
+      if (req.body.operationName === "editProfile") {
+        req.body.variables.input.email = "jace@naver.com";
+      }
+    });
     user.visit("/edit-profile");
     user.findByPlaceholderText(/email/i).clear().type("new@gmail.com");
     user.findByRole("button").click();
