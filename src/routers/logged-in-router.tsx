002414f5536d6de58d78c63aa5af05d4f0d2f875
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-04-09 16:13:49 +0900

    make Routes from Array to Object & Divide Client, Common, Ower Routes

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -9,15 +9,21 @@ import { EditProfile } from "../pages/user/edit-profile";
 import { Search } from "../pages/client/search";
 import { Category } from "../pages/client/category";
 import { Restaurant } from "../pages/client/restaurant";
+import { MyRestaurants } from "../pages/owner/my-restaurants";
 
-const ClientRoutes = () => [
-  <Route key={1} path="/" element={<Restaurants />}></Route>,
-  <Route key={2} path="/confirm" element={<ConFirmEmail />}></Route>,
-  <Route key={3} path="/edit-profile" element={<EditProfile />}></Route>,
-  <Route key={4} path="/search" element={<Search />}></Route>,
-  <Route key={5} path="/category/:slug" element={<Category />}></Route>,
-  <Route key={6} path="/restaurant/:id" element={<Restaurant />}></Route>,
+const clientRoutes = [
+  { path: "/", component: <Restaurants /> },
+  { path: "/search", component: <Search /> },
+  { path: "/category/:slug", component: <Category /> },
+  { path: "/restaurant/:id", component: <Restaurant /> },
 ];
+
+const commonRoutes = [
+  { path: "/confirm", component: <ConFirmEmail /> },
+  { path: "/edit-profile", component: <EditProfile /> },
+];
+
+const restaurantRoutes = [{ path: "/", component: <MyRestaurants /> }];
 
 export const LoggedInRouter = () => {
   const { data, loading, error } = useMe();
@@ -32,7 +38,29 @@ export const LoggedInRouter = () => {
     <Router>
       <Header email={data.me.email}></Header>
       <Routes>
-        {data.me.role === "Client" && ClientRoutes()}
+        {data.me.role === "Client" &&
+          clientRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.component}
+            ></Route>
+          ))}
+        {data.me.role === "Owner" &&
+          restaurantRoutes.map((route) => (
+            <Route
+              key={route.path}
+              path={route.path}
+              element={route.component}
+            ></Route>
+          ))}
+        {commonRoutes.map((route) => (
+          <Route
+            key={route.path}
+            path={route.path}
+            element={route.component}
+          ></Route>
+        ))}
         <Route path="/*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </Router>
