dffcd617b35bca0e9b7423e2194e7a18c51fa761
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-04-24 19:32:33 +0900

    create Dish Component

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -1,6 +1,7 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { Link, useParams } from "react-router-dom";
+import { Dish } from "../../components/dish";
 import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import {
   myRestaurant,
@@ -65,7 +66,17 @@ export const MyRestaurant = () => {
         <div className=" mt-10">
           {data?.myRestaurant.restaurant?.menu.length === 0 ? (
             <h4 className=" text-xl mb-5">Please upload a dish.</h4>
-          ) : null}
+          ) : (
+            <div className=" grid sm:grid-cols-3 gap-x-5 gap-y-10 mt-10">
+              {data?.myRestaurant.restaurant?.menu.map((dish) => (
+                <Dish
+                  name={dish.name}
+                  description={dish.description}
+                  price={dish.price}
+                ></Dish>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
