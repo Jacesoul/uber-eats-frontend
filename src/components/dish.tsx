d81844fffea689d803ca4960b0f486f5c9cd8890
Jacesoul <seolju90@gmail.com>
2022-04-28 16:59:54 +0900

    Making Order part Three

--- src/components/dish.tsx
+++ src/components/dish.tsx
@@ -9,9 +9,10 @@ interface IDishProps {
   isCustomer?: boolean;
   options?: restaurant_restaurant_restaurant_menu_options[] | null;
   orderStarted?: boolean;
+  isSelected?: boolean;
   addItemToOrder?: (dishId: number) => void;
   removeFromOrder?: (dishId: number) => void;
-  isSelected?: boolean;
+  addOptionToItem?: (dishId: number, options: any) => void;
 }
 
 export const Dish: React.FC<IDishProps> = ({
@@ -22,9 +23,10 @@ export const Dish: React.FC<IDishProps> = ({
   isCustomer = false,
   orderStarted = false,
   options,
+  isSelected,
   addItemToOrder,
   removeFromOrder,
-  isSelected,
+  addOptionToItem,
 }) => {
   const onClick = () => {
     if (orderStarted) {
@@ -38,15 +40,17 @@ export const Dish: React.FC<IDishProps> = ({
   };
   return (
     <div
-      onClick={onClick}
-      className={`  px-8 py-4 border transition-all ${
-        isSelected
-          ? "border-gray-800 cursor-not-allowed"
-          : "hover:border-gray-800 cursor-pointer"
+      className={`  px-8 py-4 cursor-pointer border transition-all ${
+        isSelected ? "border-gray-800 " : "hover:border-gray-800 "
       }`}
     >
       <div className="mb-5">
-        <h3 className=" text-lg font-medium ">{name}</h3>
+        <h3 className=" text-lg font-medium ">
+          {name}{" "}
+          {orderStarted && (
+            <button onClick={onClick}>{isSelected ? "Remove" : "Add"}</button>
+          )}
+        </h3>
         <h4 className=" font-medium">{description}</h4>
       </div>
       <span>${price}</span>
@@ -54,7 +58,17 @@ export const Dish: React.FC<IDishProps> = ({
         <div>
           <h5 className=" my-8 mb-3 font-medium">Dish Options</h5>
           {options?.map((option, index) => (
-            <span className="flex items-center" key={index}>
+            <span
+              onClick={() =>
+                addOptionToItem
+                  ? addOptionToItem(id, {
+                      name: option.name,
+                    })
+                  : null
+              }
+              className="flex border items-center"
+              key={index}
+            >
               <h6 className=" mr-2">{option.name}</h6>
               <h6 className=" text-sm opacity-75">(${option.extra})</h6>
             </span>
--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -57,19 +57,36 @@ export const Restaurant = () => {
   const triggerStartOrder = () => {
     setOrderStarted(true);
   };
+  const getItem = (dishId: number) => {
+    return orderItems.find((order) => order.dishId === dishId);
+  };
   const isSelected = (dishId: number) => {
-    return Boolean(orderItems.find((order) => order.dishId === dishId));
+    return Boolean(getItem(dishId));
   };
   const addItemToOrder = (dishId: number) => {
     if (isSelected(dishId)) {
       return;
     }
-    setOrderItems((current) => [{ dishId }, ...current]);
+    setOrderItems((current) => [{ dishId, options: [] }, ...current]);
   };
   const removeFromOrder = (dishId: number) => {
     setOrderItems((current) =>
       current.filter((dish) => dish.dishId !== dishId)
     );
+  };
+  const addOptionToItem = (dishId: number, option: any) => {
+    if (!isSelected(dishId)) {
+      return;
+    }
+    const oldItem = getItem(dishId);
+    console.log(oldItem);
+    if (oldItem) {
+      removeFromOrder(dishId);
+      setOrderItems((current) => [
+        { dishId, options: [option, ...oldItem.options!] }, // !는 typescript에게 믿어달라는 의미이다.
+        ...current,
+      ]);
+    }
   };
   console.log(orderItems);
   return (
@@ -115,6 +132,7 @@ export const Restaurant = () => {
               options={dish.options}
               addItemToOrder={addItemToOrder}
               removeFromOrder={removeFromOrder}
+              addOptionToItem={addOptionToItem}
             ></Dish>
           ))}
         </div>
