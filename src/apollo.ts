1a599d0deff2dd8790c472ffcbdc84193aa710f7
Jacesoul <seolju90@gmail.com>
2022-05-09 18:03:30 +0900

    Subscription Problem Solved using subscriptions-transport-ws

--- src/apollo.ts
+++ src/apollo.ts
@@ -3,9 +3,11 @@ import { setContext } from "@apollo/client/link/context";
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { WebSocketLink } from "@apollo/client/link/ws";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 import {
   ApolloClient,
   createHttpLink,
+  HttpLink,
   InMemoryCache,
   makeVar,
   split,
@@ -17,17 +19,11 @@ const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(token));
 export const authTokenVar = makeVar(token);
 
-// const wsLink = new GraphQLWsLink(
-//   createClient({
-//     url: `ws://localhost:4000/graphql`,
-//     connectionParams: {
-//       "x-jwt": authTokenVar() || "",
-//     },
-//   })
-// );
-
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "wss://nuber-eats-backend.herokuapp.com/graphql"
+      : `ws://localhost:4000/graphql`,
   options: {
     reconnect: true,
     connectionParams: {
@@ -37,7 +33,10 @@ const wsLink = new WebSocketLink({
 });
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri:
+    process.env.NODE_ENV === "production"
+      ? "https://nuber-eats-backend.herokuapp.com/graphql"
+      : "http://localhost:4000/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -49,9 +48,34 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+/*
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: `ws://localhost:4000/graphql`,
+    connectionParams: {
+      "x-jwt": authTokenVar() || "",
+    },
+  })
+);
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "x-jwt": authTokenVar() || "",
+    },
+  };
+});
+*/
+
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
+    console.log(definition);
     return (
       definition.kind === "OperationDefinition" &&
       definition.operation === "subscription"
--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -60,6 +60,7 @@ export const Order = () => {
       },
     }
   );
+  console.log(data);
 
   useEffect(() => {
     if (data?.getOrder.ok) {
