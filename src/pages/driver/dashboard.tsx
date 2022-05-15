a29883db9c3521fcf0a54ced3c7c3fefd45c8498
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-05-16 06:19:49 +0900

    Driver Dashboard part Three

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -8,6 +8,8 @@ interface ICoords {
 
 export const Dashboard = () => {
   const [driverCoords, setDriverCoords] = useState<ICoords>({ lng: 0, lat: 0 });
+  const [map, setMap] = useState<any>();
+  const [maps, setMaps] = useState<any>();
   const onSuccess = ({
     coords: { latitude, longitude },
   }: GeolocationPosition) => {
@@ -21,22 +23,38 @@ export const Dashboard = () => {
       enableHighAccuracy: true,
     });
   }, []);
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    }
+  }, [driverCoords.lat, driverCoords.lng]);
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
     map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng));
+    setMap(map);
+    setMaps(maps);
   };
   return (
     <div>
       <div
         className="overflow-hidden"
-        style={{ width: window.innerWidth, height: "95vh" }}
+        style={{ width: window.innerWidth, height: "50vh" }}
       >
         <GoogleMapReact
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={onApiLoaded}
-          defaultZoom={15}
+          defaultZoom={16}
           defaultCenter={{ lat: 35.58, lng: 126.95 }}
           bootstrapURLKeys={{ key: "AIzaSyC02djCnP8dNNA1zI0AvbUXIz__oi0-0UQ" }}
-        ></GoogleMapReact>
+        >
+          <div
+            //@ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+            className=" h-8 w-8 rounded-full flex justify-center items-center text-lg"
+          >
+            🚘
+          </div>
+        </GoogleMapReact>
       </div>
     </div>
   );
